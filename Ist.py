990c7b5abacfca276becb72f487173f703b6e581
Sanskar Sharma <vanssharma0090@gmail.com>
2026-07-27 20:48:13 +0530

    day 4 practice question test

--- Ist.py
+++ Ist.py
@@ -5,3 +5,13 @@ print("---your poem here---")
 print("This is a   test script.")
 import math 
 print(math.sqrt(25))
+
+## day 4 practice question test --- User Profile ---
+#Name: son 
+#Age: 
+#Is Stude
+
+name = "son"
+age =(10)
+is_student = True
+print (name ,age,is_student)

1bff7be313dbcfb3aec3731c1201006ae609d6c6
Sanskar Sharma <vanssharma0090@gmail.com>
2026-07-30 20:38:08 +0530

    Refactor Ist.py to comment out unused code and add iiird.py for 6th day practice

---
+++ iiird.py
@@ -0,0 +1,22 @@
+# # #6th day of practic 
+a = str(print("123"))
+print(a)
+# print("1234") = b 
+# print(b)
+# a = (123)
+# print(a)print
+print("Error Alert!\a")
+print("Coddd\b\bing")
+print("Python\n\tIs\n\t\tAwesome")
+print('She said, "It\'s a trap!"')
+print("Project path:\n\"D:\\NexCure\\app\"")
+# Project "NexCure"
+#     Status: Active
+print("project\"nexcure\"\n\tstatus:\tactive")
+print("studio path:  d:\\vanshraj_studio\\new_track")
+# Ek hi dabba (variable), alag-alag values!
+a = 100          # Abhi yeh Number (Integer) hai
+print(a)
+
+b= "Pass"       # Ab Python ne isko Text (String) bana diya!
+print(b)

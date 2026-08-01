c2aeadf81188c828cda87dc09e4f8107e1c944e2
Sanskar Sharma <vanssharma0090@gmail.com>
2026-08-01 20:27:43 +0530

    day_8

---
+++ day_8.py
@@ -0,0 +1,16 @@
+# # a = 25
+# # b = 5
+# # print(a/b)
+# # # c = "true" 
+# # d = 5
+# # print(c+d)
+# a = True
+# b = 10
+
+# # Python True ko implicitly 1 maan lega
+# total_points = a + b 
+# print(total_points)  # Output: 11
+a = True
+b = 5
+c = False
+print((a+b)*c)
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-cine name = "))
-# strip_quantity = int(input("enter the quantity of medicien = "))
-# price = int(input("price of medicine is = "))
-
-# total_amount = (price*strip_quantity)
-# final_bill = (total_amount - 50)
-# print(f"you purchase {medicine_name} and you get {st

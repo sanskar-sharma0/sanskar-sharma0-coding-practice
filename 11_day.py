dfba45af85fed5981d3de243b4d474c7069acfe6
Sanskar Sharma <vanssharma0090@gmail.com>
2026-08-04 19:24:46 +0530

    Add Challenge 10: The Smart Traffic Light

---
+++ 11_day.py
@@ -0,0 +1,14 @@
+# # Challenge 10: The Smart Traffic Light 🚦
+# user = input("What are the colors of a traffic light?").strip().lower()
+# if (user == "red"):
+#     print("plese stop the car ")
+# elif (user=="yellow"):
+#     print("plese start the engine and wait ")
+# elif (user=="green"):
+#     print("you can go now")
+# else:
+#     print("you enter wrong colour ")
+    
+
+
+# Challenge 11: The Smart Speed Fine Calculator 🚗💨
